7d47ea622e820cb103304b63e315bd60c30442a1
David Katalinic <david.a.katalinic@gmail.com>
2023-01-30 14:47:45 +1100

    Add project files.

---
+++ RomsetFilterApp/MainWindow.cs
@@ -0,0 +1,15 @@
+namespace RomsetFilterApp
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
